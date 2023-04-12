76675e737e49b3c29a59d2521a342ffa0070234b
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-12 20:03:19 +0600

    Button intitial class added

--- src/components/Ui/Button.tsx
+++ src/components/Ui/Button.tsx
@@ -1,10 +1,15 @@
+import { cva } from 'class-variance-authority'
 import React from 'react'
 
 type Props = {}
 
+const buttonVariants = cva(
+    'active:scale-95 in'
+)
+
 const Button = (props: Props) => {
   return (
-    <div>Button</div>
+    <div className='inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disable:opacity-50 disabled:pointer-events-none'>Button</div>
   )
 }
 
