e6d448d27396680b66854a7946081d1984ea2bd7
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-12 19:49:36 +0600

    Button added

---
+++ src/app/deshboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className='text-4xl font-bold text-white'>page</div>
+  )
+}
+
+export default page
