bae690186c87ffcf84090b548704373121c8b520
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-04-12 23:19:00 +0600

    Button props done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Button from '@/components/Ui/Button'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
    <div className='text-green-700 font-bold text-4xl'>
-    Hello Tailwind
+    <Button/>
    </div>
   )
 }
--- src/components/Ui/Button.tsx
+++ src/components/Ui/Button.tsx
@@ -1,16 +1,37 @@
-import { cva } from 'class-variance-authority'
-import React from 'react'
+import { cva, VariantProps } from 'class-variance-authority'
+import React, { ButtonHTMLAttributes, FC } from "react";
 
 type Props = {}
 
 const buttonVariants = cva(
-    'active:scale-95 in'
+    'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
+  {
+    variants: {
+      variant: {
+        default: 'bg-slate-900 text-white hover:bg-slate-800',
+        ghost: 'bg-transparent hover:text-slate-900 hover:bg-slate-200',
+      },
+      size: {
+        default: 'h-10 py-2 px-4',
+        sm: 'h-9 px-2',
+        lg: 'h-11 px-8',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
 )
 
-const Button = (props: Props) => {
-  return (
-    <div className='inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disable:opacity-50 disabled:pointer-events-none'>Button</div>
-  )
-}
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
 
-export default Button
+
+
+interface IProps {};
+
+const Button:FC<IProps> = (props) => {
+  return <div>Button</div>
+};
+
+export default Button;
